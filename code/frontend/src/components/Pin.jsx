962a9b314e3f3b55baf05048c9afb4efed2de142
annakubiak <89855163+kubiakanna@users.noreply.github.com>
2022-01-24 19:01:05 +0100

    add the pin creating feature

--- code/frontend/src/components/Pin.jsx
+++ code/frontend/src/components/Pin.jsx
@@ -89,12 +89,13 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
                             {destination && (
                                 <a
                                     href={destination}
+                                    // how to stop propagation after clicking the link
                                     target='_blank'
                                     rel='noreferrer'
                                     className='bg-white flex items-center gap-2 text-black font-bold p-2 pl-4 pr-4 rounded-full opacity-70 hover:opacity-100 hover:shadow-md'
                                 >
                                     <BsFillArrowUpRightCircleFill />
-                                    {destination.length > 20 ? destination.slice(8, 20) : destination.slice(8)}
+                                    {destination.length > 25 ? destination.slice(12, 22) : destination.slice(12)}
                                 </a>
                             )}
                             {postedBy?._id === user.googleId && (
